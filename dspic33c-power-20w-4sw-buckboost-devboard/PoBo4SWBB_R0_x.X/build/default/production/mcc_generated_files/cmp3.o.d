67f9ec8fa4ee0591d0052bebdc6f4796fa5c95e3
Microchip Technology <microchip@microchip.com>
2020-11-09 14:30:09 +0530

    Updated projects

--- dspic33c-power-20w-4sw-buckboost-devboard/PoBo4SWBB_R0_x.X/build/default/production/mcc_generated_files/cmp3.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/mcc_generated_files/cmp3.o:  \
- mcc_generated_files/cmp3.c  \
-mcc_generated_files/cmp3.h 
--- dspic33c-power-20w-4sw-buckboost-devboard/PoBo4SWBB_R0_x.X/build/default/production/mcc_generated_files/uart1.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/mcc_generated_files/uart1.o:  \
- mcc_generated_files/uart1.c  \
-mcc_generated_files/uart1.h  \
- sources/device/dev_uart_comm.h 
